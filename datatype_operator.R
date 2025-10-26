15896e824932714f1e189fde495ad7ab70703335
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2025-10-26 21:00:41 +0600

    operator and datatype

---
+++ datatype_operator.R
@@ -0,0 +1,57 @@
+#Datatype ####
+
+typeof(6)
+typeof(3L)
+typeof(TRUE)
+typeof(T)
+typeof("cat")
+typeof(3i+3)
+
+
+typeof(c(8,1,4))
+typeof(c(5L,3L,6L))
+
+
+mode(c(5L,3L,6L))
+
+
+
+#value assign ####
+x<-7
+y<-9
+z<-10
+
+
+a<-c(23,2,5,6)
+print(a)
+
+age<-a
+
+rm(a) #just delete environment
+
+
+b <-1:100
+b
+b[33]
+
+
+
+
+mat1 <-matrix(c(1,2,3,4,5,6),ncol=3,nrow=2)
+mat1
+ 
+
+mat2 <-matrix(c(1,2,3,4,5,6),ncol=3,nrow=2)
+mat2
+
+mat1%%mat2
+
+
+
+
+
+
+
+
+
+
