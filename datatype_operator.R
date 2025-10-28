38cd22534eb9b23e22f405ad4dac7cfe7d16b9b8
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2025-10-28 11:51:31 +0600

    update

--- datatype_operator.R
+++ datatype_operator.R
@@ -37,14 +37,48 @@ b[33]
 
 
 
-mat1 <-matrix(c(1,2,3,4,5,6),ncol=3,nrow=2)
-mat1
- 
+x1 <-c("apple", "nokia ", " samsung", "xiomi")
+x1
 
-mat2 <-matrix(c(1,2,3,4,5,6),ncol=3,nrow=2)
-mat2
+#vivo check in x1
+"vivo"%in%x1
+"apple"%in%x1
 
-mat1%%mat2
+
+ #Relational
+x<-c(T,F)
+y <-c(F,T)
+
+x|y
+x&y
+
+#function
+d<-c(1,2,3,NA)
+
+sum(d)
+mean(d)
+summary(d)
+which(is.na(x))
+any(is.na(x))
+any(c(T,T,T,T,F,F))#atleast one true
+all(c(F,F,F,F))  #all are F
+
+
+
+
+
+
+#Matrix ####
+
+thisMatrix <-matrix(c(1,2,3,4,5,6), nrow=3, ncol=3)
+thisMatrix
+thisMatrix[2,2]
+thisMatrix[2,]
+thisMatrix[,1]
+newmatrix <- cbind(thisMatrix,c(7,8,9,10,11,12))
+newmatrix
+
+
 
 
 
